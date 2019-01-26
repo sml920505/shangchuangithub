d96b5f03319766a72739fe54c861e886c284ea6e
song ming liang <davidsongtufe@gmail.com>
2019-01-26 09:31:06 +0800

    buduo 2

---
+++ play.js
@@ -0,0 +1,10 @@
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://labs.nearpod.com/bodymovin/demo/markus/isometric/markus2.json'
+  // path: './2.json'
+})
+console.log("play")
